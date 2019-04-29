8239cb0ea782f69d0bb5d9771c46f150bcf71ea0
RemRanger <rxb@ziggo.nl>
2019-04-29 09:04:30 +0200

    Fixed hash router.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 ﻿import React from "react";
-import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+import { HashRouter, Route, NavLink } from "react-router-dom";
 import Home from "./Home";
 import LocationList from "./LocationList";
 import LocationDetail from "./LocationDetail";
@@ -11,31 +11,33 @@ import SessionDetail from "./SessionDetail";
 function App()
 {
     return (
-        <Router>
-            <div style={{ textAlign: 'center' }} >
-                <ul className='topnav'>
-                    <li>
-                        <img src="favicon.ico" alt="Icon" style={{ verticalAlign: 'middle', height: '32px' }} />
-                        <span style={{ color: 'skyblue', fontWeight: '800' }}>&nbsp;&nbsp;&Xi;SC&Lambda;L&Lambda;D&Lambda;&nbsp;</span>
-                    </li>
-                    <li><NavLink to="/home">Home</NavLink></li>
-                    <li><NavLink to="/locations">Locations</NavLink></li>
-                    <li><NavLink to="/climbers">Climbers</NavLink></li>
-                    <li><NavLink to="/about">About</NavLink></li>
-                    <li><NavLink to="/login">Login</NavLink></li>
-                </ul>
-            </div>
+        <HashRouter>
+            <div>
+                <div style={{ textAlign: 'center' }} >
+                    <ul className='topnav'>
+                        <li>
+                            <img src="favicon.ico" alt="Icon" style={{ verticalAlign: 'middle', height: '32px' }} />
+                            <span style={{ color: 'skyblue', fontWeight: '800' }}>&nbsp;&nbsp;&Xi;SC&Lambda;L&Lambda;D&Lambda;&nbsp;</span>
+                        </li>
+                        <li><NavLink to="/home">Home</NavLink></li>
+                        <li><NavLink to="/locations">Locations</NavLink></li>
+                        <li><NavLink to="/climbers">Climbers</NavLink></li>
+                        <li><NavLink to="/about">About</NavLink></li>
+                        <li><NavLink to="/login">Login</NavLink></li>
+                    </ul>
+                </div>
 
-            <Route exact path="/" component={Home} />
-            <Route path="/home" component={Home} />
-            <Route exact path="/locations" component={LocationList} />
-            <Route path="/locations/:id" component={LocationDetail} />
-            <Route path="/climbers" component={UserList} />
-            <Route path="/about" component={About} />
-            <Route path="/login" component={Login} />
-            {/*<Route exact path="/sessions" component={SessionList} />*/}
-            <Route path="/sessions/:id/:userId" component={SessionDetail} />
-        </Router>
+                <Route exact path="/" component={Home} />
+                <Route path="/home" component={Home} />
+                <Route exact path="/locations" component={LocationList} />
+                <Route path="/locations/:id" component={LocationDetail} />
+                <Route path="/climbers" component={UserList} />
+                <Route path="/about" component={About} />
+                <Route path="/login" component={Login} />
+                {/*<Route exact path="/sessions" component={SessionList} />*/}
+                <Route path="/sessions/:id/:userId" component={SessionDetail} />
+            </div>
+        </HashRouter>
     );
 }
 
